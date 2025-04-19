3a0967b5ecda27103eb6038f8ead41ceee4edd96
11dpowergirls <atamanukvala450@gmail.com>
2025-04-19 16:15:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openArticles(){
+
+    window.location.href = "index.html";
+    
+    }
+    
+    function openContacts(){ 
+    
+    window.location.href ="contacts.html";
+    
+    }
+    
+    function openReviews(){
+    
+    window.location.href = "reviews.html";
+    
+    }
